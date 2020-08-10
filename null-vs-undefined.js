2e92577eb084075696c17436f1bb6e9a4c79044e
tamim-islam <67605624+tamim-islam@users.noreply.github.com>
2020-08-11 00:07:35 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+// let pakhi;
+// console.log(pakhi)
+
+// function add(num1, num2){
+//     console.log(num1 + num2)
+//     return
+// // }
+// function add(num1, num2){
+//     console.log(num1 , num2)
+   
+// }
+
+// const premik = {name: "smart dude",phone:458421};
+// console.log(premik.gf);
+let fun = undefined;
+console.log(fun)
+
+null
+let ages =[2,5,6];
+console.log(ages[11])
