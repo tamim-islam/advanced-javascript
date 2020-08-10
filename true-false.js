ae46994661b5237edc4645b19ed606f7bd901dcd
tamim-islam <67605624+tamim-islam@users.noreply.github.com>
2020-08-10 22:14:00 +0600

    true or false

---
+++ true-false.js
@@ -0,0 +1,13 @@
+// 0
+// ""
+// undefined
+// null
+
+ let name=0;
+console.log(name)
+ if(name || name==0){
+     console.log( "condition is true")
+ }
+ else{
+     console.log("condition is false")
+ }
